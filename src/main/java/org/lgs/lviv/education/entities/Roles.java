eee92023d4cc8d8070d7bea3004b785463cd869d
hummai <shining.melon2015@gmail.com>
2020-04-30 14:20:03 +0300

    Edited user entity

--- src/main/java/org/lgs/lviv/education/entities/Roles.java
+++ src/main/java/org/lgs/lviv/education/entities/Roles.java
@@ -1,5 +1,12 @@
 package org.lgs.lviv.education.entities;
 
-public enum Roles {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Roles implements GrantedAuthority {
     ENROLLEE, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
