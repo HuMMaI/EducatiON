86066561ccf5f9e0810d8422c788a725c4d45f34
hummai <shining.melon2015@gmail.com>
2020-04-30 14:23:21 +0300

    Added user services

---
+++ src/main/java/org/lgs/lviv/education/services/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.lgs.lviv.education.services;
+
+import org.lgs.lviv.education.entities.User;
+import org.lgs.lviv.education.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userMaybe = userRepository.findByEmail(email);
+        return userMaybe
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("No user present with email: " + email));
+    }
+}
