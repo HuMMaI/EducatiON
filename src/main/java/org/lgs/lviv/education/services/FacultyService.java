b3ba2b1fffdbfaf975e610cef2fcb341bfc0b571
hummai <shining.melon2015@gmail.com>
2020-04-30 20:32:12 +0300

    Added faculty service and repo

---
+++ src/main/java/org/lgs/lviv/education/services/FacultyService.java
@@ -0,0 +1,30 @@
+package org.lgs.lviv.education.services;
+
+import org.lgs.lviv.education.entities.Faculty;
+import org.lgs.lviv.education.repositories.FacultyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FacultyService {
+    private final FacultyRepository facultyRepository;
+
+    @Autowired
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public List<Faculty> findAll(){
+        return facultyRepository.findAll();
+    }
+
+    public void save(Faculty faculty) {
+        facultyRepository.save(faculty);
+    }
+
+    public void create(Faculty faculty) {
+        facultyRepository.save(faculty);
+    }
+}
