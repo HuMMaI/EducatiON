e9db58ef880f61054462152f2b0a5e0eb0ffd8ef
hummai <shining.melon2015@gmail.com>
2020-05-06 20:17:37 +0300

    Edited facultyEdit method

--- src/main/java/org/lgs/lviv/education/controllers/FacultyController.java
+++ src/main/java/org/lgs/lviv/education/controllers/FacultyController.java
@@ -1,6 +1,8 @@
 package org.lgs.lviv.education.controllers;
 
 import org.lgs.lviv.education.entities.Faculty;
+import org.lgs.lviv.education.entities.FacultySubjects;
+import org.lgs.lviv.education.entities.Roles;
 import org.lgs.lviv.education.services.FacultyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,6 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/faculty")
@@ -27,19 +33,34 @@ public class FacultyController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String facultyEditForm(@PathVariable Faculty faculty, Model model){
         model.addAttribute("faculty", faculty);
+        model.addAttribute("subjects", FacultySubjects.values());
 
         return "facultyEditPage";
     }
 
+    //todo make dto
     @PostMapping
     @PreAuthorize("hasAuthority('ADMIN')")
     public String facultyEdit(
             @RequestParam("id") Faculty faculty,
             @RequestParam String name,
-            @RequestParam int numberOfSeats
+            @RequestParam int numberOfSeats,
+            @RequestParam Map<String, String> form
     ){
         faculty.setName(name);
         faculty.setNumberOfSeats(numberOfSeats);
+
+        faculty.getSubjects().clear();
+
+        Set<String> subjects = Arrays.stream(FacultySubjects.values())
+                .map(FacultySubjects::name)
+                .collect(Collectors.toSet());
+
+        for (String key : form.keySet()) {
+            if (subjects.contains(key)){
+                faculty.getSubjects().add(FacultySubjects.valueOf(key));
+            }
+        }
 
         facultyService.save(faculty);
 
@@ -58,5 +79,12 @@ public class FacultyController {
         facultyService.create(faculty);
 
         return "redirect:/faculty";
+    }
+
+    @GetMapping("/info")
+    public String facultyInfo(@RequestParam("id") Faculty faculty, Model model){
+        model.addAttribute("faculty", faculty);
+
+        return "facultyInfo";
     }
 }
